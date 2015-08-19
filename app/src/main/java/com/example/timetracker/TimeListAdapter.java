c4aa3c8a57d94e0073473f3082c531f5b97200f3
xybCoder <1771137609@qq.com>
2015-08-19 09:51:55 +0800

    The first commit of timetracker

---
+++ app/src/main/java/com/example/timetracker/TimeListAdapter.java
@@ -0,0 +1,36 @@
+package com.example.timetracker;
+
+import android.content.Context;
+import android.text.format.DateUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class TimeListAdapter extends ArrayAdapter<Long> {
+    
+    public TimeListAdapter(Context context, int textViewResourceId) {
+        super(context, textViewResourceId);
+    }
+    
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            view = LayoutInflater.from(getContext()).inflate(R.layout.time_row, null);
+        }
+        
+        long time = getItem(position);
+        
+        TextView name = (TextView) view.findViewById(R.id.lap_name);
+        String taskString = getContext().getResources().getString(R.string.task_name);
+        name.setText(String.format(taskString, position+1));
+        
+        TextView lapTime = (TextView) view.findViewById(R.id.lap_time);
+        String milliSecond = String.format(":%03d",time%1000);
+        lapTime.setText(DateUtils.formatElapsedTime(time/1000)+milliSecond);
+        
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/timetracker/Util.java
@@ -0,0 +1,23 @@
+package com.example.timetracker;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Build;
+
+public class Util {
+    public static boolean isDebugMode(Context context) {
+        PackageManager pm = context.getPackageManager();
+        try {
+            ApplicationInfo info = pm.getApplicationInfo(context.getPackageName(), 0);
+            return (info.flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
+        } catch (NameNotFoundException e) {
+        }
+        return true;
+    }
+    
+    public static boolean useStrictMode(Context context) {
+        return isDebugMode(context) && Build.VERSION.SDK_INT >= 9;
+    }
+}
